8341e32b7b890e0c4a66eed4554219735ef7e96c
weeee9 <teletubby332@gmail.com>
2023-08-22 21:41:58 +0800

    feat: convert markdown files to HTML and handle errors
    - Convert markdown files by given templates
    - Save converted markdown files to given path
    - Fix a bug where the program would re-generate files even if they haven't changed
    - Add support for converting markdown files to HTML
    - Handle errors when reading, converting, and writing markdown files
    - Update file extension to HTML after conversion
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- README.md
+++ README.md
@@ -4,8 +4,9 @@ A little project inspired by [tinyprojects.dev](https://tinyprojects.dev)
 
 ## Things todo
 - [x] read markdown files from given path
-- [ ] convert markdown files
-- [ ] save them to given path
+- [x] convert markdown files
+- [ ] convert markdown files by given templates
+- [x] save them to given path
 - [ ] tracking files converting record, so we won't re-gen when file's not changed
 - [ ] make it an github action to to the job automatically.
   - [ ] gen html files
--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/joho/godotenv"
 	"github.com/mattn/go-isatty"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"github.com/russross/blackfriday/v2"
 	"github.com/urfave/cli/v2"
 )
 
@@ -54,6 +56,7 @@ func main() {
 
 func run(c *cli.Context) error {
 	source := c.String("source")
+	destination := c.String("destination")
 
 	entries, err := os.ReadDir(source)
 	if err != nil {
@@ -69,6 +72,41 @@ func run(c *cli.Context) error {
 		}
 
 		log.Info().Str("filename", filename).Msg("processing markdown file")
+		if err := convert(filename, source, destination); err != nil {
+			log.Error().Err(err).Str("filename", filename).Msg("failed to convert markdown file")
+			return err
+		}
+		log.Info().Str("filename", filename).Msg("markdown file converted")
+	}
+
+	return nil
+}
+
+func convert(filename, src, dst string) error {
+	realPath := filepath.Join(src, filename)
+	md, err := os.ReadFile(realPath)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to read markdown file")
+		return err
+	}
+
+	renderer := blackfriday.NewHTMLRenderer(
+		blackfriday.HTMLRendererParameters{
+			Flags: blackfriday.CommonHTMLFlags &^ blackfriday.Smartypants,
+		},
+	)
+
+	html := blackfriday.Run(md, blackfriday.WithRenderer(renderer))
+
+	if err := os.MkdirAll(dst, os.ModePerm); err != nil {
+		log.Error().Err(err).Msg("failed to create destination folder")
+		return err
+	}
+
+	outputPath := filepath.Join(dst, replaceFileExtensionToHTML(filename))
+	if err := os.WriteFile(outputPath, html, os.ModePerm); err != nil {
+		log.Error().Err(err).Msg("failed to write html file")
+		return err
 	}
 
 	return nil
@@ -84,4 +122,20 @@ func isMarkdownExtension(ext string) bool {
 		return true
 	}
 	return false
+}
+
+func getFilename(path string) string {
+	return filepath.Base(path)
+}
+
+func replaceFileExtensionToHTML(filename string) string {
+	return trimMarkdownExtension(getFilename(filename)) + ".html"
+}
+
+func trimMarkdownExtension(filename string) string {
+	if !isMarkdownFile(filename) {
+		return filename
+	}
+
+	return strings.TrimSuffix(filename, filepath.Ext(filename))
 }
